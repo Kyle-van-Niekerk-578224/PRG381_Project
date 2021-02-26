e02fdd5c3385f3a52cb0632fe1a096f7e22f7230
Kyle-van-Niekerk-578224 <578224@student.belgiumcampus.ac.za>
2021-02-26 10:32:30 +0200

    Init

---
+++ BusinessLogicLayer/Event.java
@@ -0,0 +1,63 @@
+package BusinessLogicLayer;
+
+abstract class Event{
+
+    char[] type;        //This might be easier to work with using databases than converting strings
+	float price = 5;	//value needs to be decided upon
+	Venue eventVenue;
+    int people;
+
+    public boolean checkBookingDay(int days){
+         if (days > 15)
+         {
+             return true;
+         }
+         else return false;
+    }
+        //this will have the invoice multiply the  price of ordered adult meals by 0.85.
+
+    public boolean checkPeople(int people){
+        if (people < 50)
+        {
+            return true;
+        }
+        else return false;
+        //this will have the invoice multiply the total price by 0.5 if 15 or fewer day remain.
+    }
+
+    //Getters and setters
+    public char[] getType() {
+        return type;
+    }
+
+    public void setType(char[] type) {
+        this.type = type;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Venue getEventVenue() {
+        return eventVenue;
+    }
+
+    public void setEventVenue(Venue eventVenue) {
+        this.eventVenue = eventVenue;
+    }
+
+    public int getPeople() {
+        return people;
+    }
+
+    public void setPeople(int people) {
+		this.people = people;
+	}
+}
+
+
+
---
+++ BusinessLogicLayer/Venue.java
@@ -0,0 +1,34 @@
+package BusinessLogicLayer;
+
+public class Venue{
+	private char[] name;
+	private char[] address;
+	private char[] number;
+
+	//This class only contains getters and setters
+
+	public char[] getName() {
+		return name;
+	}
+
+	public void setName(char[] name) {
+		this.name = name;
+	}
+
+	public char[] getAddress() {
+		return address;
+	}
+
+	public void setAddress(char[] address) {
+		this.address = address;
+	}
+
+	public char[] getNumber() {
+		return number;
+	}
+
+	public void setNumber(char[] number) {
+		this.number = number;
+	}
+	
+}
