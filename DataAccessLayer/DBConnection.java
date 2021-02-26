5ef72e53aa33684a457bb95f726135052978f957
ItzJustSomeGuy <sebastiaan.wolfaardt@gmail.com>
2021-02-26 11:17:54 +0200

    updated DBConnection class with basic code

--- DataAccessLayer/DBConnection.java
+++ DataAccessLayer/DBConnection.java
@@ -2,10 +2,20 @@ package DataAccessLayer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DBConnection {
-    //WIP
+    public static void main(String[] args) {
+
+        //example string, proper string will be added when database is finalised
+        String connectionUrl = "jdbc:sqlserver://yourserver.database.windows.net:1433;" + "database=AdventureWorks;"
+                + "user=yourusername@yourserver;" + "password=yourpassword;" + "encrypt=true;"
+                + "trustServerCertificate=false;" + "loginTimeout=30;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
